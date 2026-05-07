7a5e438f89fd3e3fce1b2b45753c8dc0f6b86cf0
lukasdannemann <lukasdannemann@outlook.com>
2026-05-07 11:25:05 +0200

    add price validation to productSchema and products.test.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,24 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'Product name is required'],
+
+      required: [true, "Product name is required"],
+
       trim: true,
     },
+
     price: {
       type: Number,
-      required: [true, 'Product price is required'],
+
+      required: [true, "Product price is required"],
+
+      min: [0, "Price must be equal to 0 or higher"],
     },
+
     description: {
       type: String,
-      default: '',
+
+      default: "",
     },
   },
+
   {
     timestamps: true,
-  }
+  },
 );
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
