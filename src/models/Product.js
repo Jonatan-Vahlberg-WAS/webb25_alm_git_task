e06fc16c5029be2f4cacbf1644a2b54f11027cb3
ddaviddahlgren <ddaviddahlgren@gmail.com>
2026-05-07 10:38:00 +0200

    added name validator

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'Product name is required'],
+      required: [true, "Product name is required"],
       trim: true,
+      validate: {
+        validator: (name) => name.length < 1,
+        message: "Your name must contain at least one letter",
+      },
     },
     price: {
       type: Number,
-      required: [true, 'Product price is required'],
+      required: [true, "Product price is required"],
     },
     description: {
       type: String,
-      default: '',
+      default: "",
     },
   },
   {
     timestamps: true,
-  }
+  },
 );
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
