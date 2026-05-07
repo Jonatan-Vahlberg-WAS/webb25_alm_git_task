e5575eb72fe3b02206383f69c2cbc7a7acfb95a7
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-07 14:11:56 +0200

    feat: implement name, description and isActive fields in categorySchema

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,8 +1,23 @@
-const mongoose = 'mongoose';
+const mongoose = require('mongoose')
 
 const categorySchema = new mongoose.Schema(
-    {
-
+  {
+    name: {
+      type: String,
+      require: [true, 'Category is required'],
+      trim: true
+    },
+    description: {
+      type: String,
+      require: true,
+      default: ''
+    },
+    isActive: {
+      type: Boolean,
+      default: true
     }
+  },
+  { timestamps: true }
 )
-module.exports = mongoose.model('Category', categorySchema) 
+
+module.exports = mongoose.model('Category', categorySchema)
