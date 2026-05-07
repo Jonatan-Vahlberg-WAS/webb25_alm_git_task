84e17d5263a0be628307f14e1b3c3a916aeb88d0
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-07 14:35:29 +0200

    feat: implement getCaegory function

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,4 +1,17 @@
-const getCategory = () => {}
+const Category = require('./models/Category')
+
+const getCategory = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        if(!category) {
+            res.status(404).json({ message: "Categories not found"});
+        }
+        res.status(200).json(categories);
+    } catch (error) {
+        res.status(500).json({ message: "Could not fetch categories"})
+    }
+}
+
 const getCategoryById = () => {}
 const createCategory = () => {}
 const updateCategory = () => {}
