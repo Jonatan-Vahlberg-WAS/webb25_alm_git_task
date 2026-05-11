a6c54a41e27a2893f86e5081a29216e090a727c5
ddaviddahlgren <ddaviddahlgren@gmail.com>
2026-05-11 10:35:10 +0200

    added duplicate email validation

--- src/models/User.js
+++ src/models/User.js
@@ -1,55 +1,69 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt")
-const { isEmail } = require("validator")
+const bcrypt = require("bcrypt");
+const { isEmail } = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, "A first name is required"],
-    trim: true,
-    minLength: 3,
-    maxLength: 30,
-    validate: {
-      validator: () => !this.name,
-      message: "A first name is required",
-    },
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    minLength: 3,
-    maxLength: 30,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
-    validate: {
-      validator: function (email) {
-        return isEmail(email);
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "A first name is required"],
+      trim: true,
+      minLength: 3,
+      maxLength: 30,
+      validate: {
+        validator: () => !this.name,
+        message: "A first name is required",
       },
-      message: (props) => `${props.value} is not a valid email`,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      minLength: 3,
+      maxLength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      validate: {
+        validator: function (email) {
+          return isEmail(email);
+        },
+        message: (props) => `${props.value} is not a valid email`,
+      },
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+      select: false,
     },
   },
-  passwordHash: {
-    type: String,
-    required: true,
-    select: false
-  },
-}, { timestamps: true }
-)
+  { timestamps: true },
+);
 
 userSchema.pre("save", async function (next) {
-    if (this.isModified("passwordHash")) {
-        const salt = await bcrypt.genSalt(10)
-        this.passwordHash = await bcrypt.hash(this.passwordHash, salt)
-    }
-    next()
+  if (this.isModified("passwordHash")) {
+    const salt = await bcrypt.genSalt(10);
+    this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
+  }
+  next();
 });
 
-const User = mongoose.model("User", userSchema)
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("email")) {
+    const existingUser = await mongoose
+      .model("User")
+      .findOne({ email: this.email });
+    if (existingUser) {
+      return next(new Error(`Email is already in use`));
+    }
+    next();
+  }
+});
 
-export default User
+const User = mongoose.model("User", userSchema);
+
+export default User;
