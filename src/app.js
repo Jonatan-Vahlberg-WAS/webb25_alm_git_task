8743f74f8fc342441568e73ed13efff4abb9ea4b
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2026-05-11 08:34:25 +0200

    implement a new frontend

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+const cors = require('cors')
 const express = require('express')
 const productRoutes = require('./routes/productRoutes')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/products', productRoutes)
 
