65c4a96c0cfad861691743cf0bc7fde45b9f0bbf
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2026-05-11 10:26:28 +0200

    implement mongoose debug logs

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ dotenv.config()
 
 const PORT = process.env.PORT || 3000
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/product_api'
+const DEBUG = ['1', 'true', 'yes'].includes(String(process.env.DEBUG).toLowerCase())
+
+mongoose.set('debug', DEBUG)
 
 mongoose
   .connect(MONGODB_URI)
