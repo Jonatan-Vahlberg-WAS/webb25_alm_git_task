86366ea1845fe75b16aa9b6340044682a382feea
codedcollections <ma.unander@gmail.com>
2026-05-11 11:01:12 +0200

    Add user model and GET and POST routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const cors = require('cors')
 const express = require('express')
 const productRoutes = require('./routes/productRoutes')
 const categoryRoutes = require('./routes/categoryRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/products', productRoutes)
 app.use('/categories', categoryRoutes)
+app.use('/users', userRoutes)
 
 app.get('/', (req, res) => {
   res.json({
