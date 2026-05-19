b906e2095274e33368ba4379b3baa897a52ec0b0
Desirée Strand <desireestrxnd@gmail.com>
2026-05-19 13:10:00 +0200

    add mail model hook with auto generated mail content

--- src/models/Mail.js
+++ src/models/Mail.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const User = require('./User')
 
 const mailSchema = new mongoose.Schema(
   {
@@ -25,5 +26,21 @@ const mailSchema = new mongoose.Schema(
     timestamps: true
   }
 )
+
+mailSchema.pre('save', async function (next) {
+  if (this.status === 'welcome') {
+    const user = await mongoose.model('User').findById(this.user)
+
+    if (user?.name) {
+      this.subject = `Välkommen ${user.name}! Ditt konto är skapat`
+      this.content = `Hej ${user.name}, välkommen! Ditt konto med e-postadressen ${user.email} har skapats. Vi är glada att ha dig med oss.`
+    } else {
+      this.subject = 'Välkommen! Ditt konto är skapat'
+      this.content = `Hej, välkommen! Ditt konto med e-postadressen ${user.email} har skapats. Vi är glada att ha dig med oss.`
+    }
+  }
+  
+  next()
+})
 
 module.exports = mongoose.model('Mail', mailSchema)
