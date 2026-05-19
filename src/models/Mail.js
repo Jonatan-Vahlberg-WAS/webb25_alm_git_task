528c1bd440c6136f7f5d42ca2584d15b79e93323
Desirée Strand <desireestrxnd@gmail.com>
2026-05-19 12:33:44 +0200

    add mail model

---
+++ src/models/Mail.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const mailSchema = new mongoose.Schema(
+  {
+    subject: {
+      type: String
+    },
+    status: {
+      type: String,
+      enum: ['welcome']
+    },
+    content: {
+      type: String
+    },
+    sentAt: {
+      type: Date,
+      default: Date.now
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('Mail', mailSchema)
