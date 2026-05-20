78abdebf621576dc2e0f409d113a8ba09b7303f2
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-20 08:23:17 +0200

    feat: create models/Mail.js and implement mailSchema

---
+++ src/models/Mail.js
@@ -0,0 +1,35 @@
+const mongoode = require('mongoose')
+
+const mailSchema = new mongoose.Schema(
+  {
+    subject: {
+      type: String,
+      required: [true, 'Subject is required'],
+      trim: true
+    },
+    status: {
+      type: String,
+      enum: ['welcome'],
+      default: 'welcome',
+      required: [true, 'Status is required']
+    },
+    content: {
+      type: String,
+      required: [true, 'Content is required']
+    },
+    sentAt: {
+      type: Date,
+      default: Date.now
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'User reference is required']
+    }
+  },
+  { 
+    timestamps: true 
+  }
+);
+
+module.exports = mongoose.model('Mail', mailSchema)
